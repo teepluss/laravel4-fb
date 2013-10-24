5ab449a36d384a0a17560ed999b231af01237aaf
Tee <admin@jquerytips.com>
2013-10-24 11:44:28 +0700

    change config

--- src/config/config.php
+++ src/config/config.php
@@ -1,15 +1,9 @@
 <?php
 
-// Production
-/*return array(
-    'appId' => '649515401746384',
-    'secret' => 'c6f279215328e9696dae2fd2bdb5b171'
-);*/
-
 return array(
 
-    'appId' => '210702942435152',
+    'appId'  => '[FACEBOOK-APP-ID]',
 
-    'secret' => '248b3e2e98f99178b091ff7db6267116'
+    'secret' => '[FACEBOOK-SECRET'
 
 );
