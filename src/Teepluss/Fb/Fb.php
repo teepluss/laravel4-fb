544ccaa300a3187e6e7c280c60e12d03c6b99b88
Tee <admin@jquerytips.com>
2013-10-25 18:16:40 +0700

    facebook sdk extended

--- src/Teepluss/Fb/Fb.php
+++ src/Teepluss/Fb/Fb.php
@@ -4,9 +4,48 @@ use Facebook as BaseFacebook;
 
 class Fb extends BaseFacebook {
 
+    /**
+     * Construct.
+     *
+     * @param array $config
+     */
     public function __construct($config)
     {
         parent::__construct($config);
+    }
+
+    /**
+     * Checking permissions.
+     *
+     * @param  mixed    $perms
+     * @param  integer  $uid
+     * @return boolean
+     */
+    public function hasPermissions($perms, $uid)
+    {
+        if ( ! is_array($perms))
+        {
+            $perms = explode(',', $perms);
+        }
+
+        // Get permissions from graph api.
+        $permissions = $this->api('/'.$uid.'/permissions');
+
+        $currentPermissions = current(array_get($permissions, 'data'));
+
+        $has = true;
+
+        // Checking process.
+        foreach ($perms as $perm)
+        {
+            if ( ! array_key_exists($perm, $currentPermissions) or $currentPermissions[$perm] == 0)
+            {
+                $has = false;
+                break;
+            }
+        }
+
+        return $has;
     }
 
 }
